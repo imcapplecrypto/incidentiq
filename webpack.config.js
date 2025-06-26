a8d22207e8096b23798e93b44f8b53bed9302b91
imcapplecrypto <chris@alphasociety.com>
2025-06-25 23:39:50 -0400

    Additional tweaks to webpack.config.js.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 // webpack.config.js
-const { createExpoWebpackConfigAsync } = require('expo/webpack-config');
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = async function(env, argv) {
