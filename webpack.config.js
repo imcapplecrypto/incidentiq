483e843e7a084300eba62434f2e700656ab569e2
imcapplecrypto <chris@alphasociety.com>
2025-06-25 21:35:53 -0400

    Resolving issue with webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,16 @@
-// webpack.config.cjs
+// webpack.config.js
 const { createExpoWebpackConfigAsync } = require('@expo/webpack-config');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = async function (env, argv) {
-  // 1. grab the default Expo config
   const config = await createExpoWebpackConfigAsync(env, argv);
 
-  // 2. turn on ESM output
+  // 1) Emit an ESM bundle so import.meta is legal:
   config.output.module = true;
   config.experiments = config.experiments || {};
   config.experiments.outputModule = true;
 
-  // 3. force HtmlWebpackPlugin to use type="module"
+  // 2) Inject <script type="module"> in your index.html
   config.plugins = config.plugins.map((plugin) => {
     if (plugin instanceof HtmlWebpackPlugin) {
       plugin.userOptions.scriptLoading = 'module';
